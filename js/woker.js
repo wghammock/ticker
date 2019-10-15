9ab1d51614f829b58cb7764d974e62bd3f907928
critter81 <CR@ServiceManagerPro.com>
2019-10-15 16:24:53 -0700

    final strech

--- js/woker.js
+++ js/woker.js
@@ -1,22 +1,34 @@
+// This needs to be global, so stop meassages can effect it.
+var feed_timer;
+
 // Setup an event listener that will handle messages sent to the worker.
 self.addEventListener('message', function(e) {
-    // Send the message back.
-    //self.postMessage('You said: ' + e.data);
 
-    console.log('whiterabbit');
-    console.log(e.data);
+    // console.log(e.data);
+    // console.log(e.data.pos + ' | ' + e.data.speed);
+    console.log(e.data.stopped);
 
-    let num = e.data;
+    let num = parseFloat(e.data.pos).toFixed(3);
+    let speed = parseFloat(e.data.speed).toFixed(3);
 
-    while (num < 10) {
-        self.postMessage('You said:' + num);
-        num ++;
+    if (e.data.stopped === 1) {
+        clearInterval(feed_timer);
+        self.close();
+    } else {
+        feed_timer = setInterval(frame.bind(this), 15);
     }
 
-
-    // obj = JSON.parse(JSON.stringify(obj));
-    // parent.postMessage(obj, 'whatever');
-
+    function frame() {
+        if ((num < -100) || (e.data.stopped === 1)) {
+            self.postMessage(num);
+            clearInterval(feed_timer);
+        } else {
+            // TODO
+            // CHECK DIRECTION WOULD GO HERE
+            num = (num - speed);
+            self.postMessage(num);
+        }
+    }
 
 
 }, false);
